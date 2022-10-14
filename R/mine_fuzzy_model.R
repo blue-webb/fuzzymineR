84c13fbf00db1fcb0dbcc7670979c0ceecf0981a
blue-webb <bwebb@air.org>
2022-10-14 12:18:03 -0500

    update

--- R/mine_fuzzy_model.R
+++ R/mine_fuzzy_model.R
@@ -86,6 +86,9 @@
 #'
 #' @export
 
+stop_eventlog <- function(eventlog)
+  if(!("eventlog" %in% class(eventlog)))
+    stop("Function only applicable for class eventlog")
 
 createXES<- function(file,
                      traces,
